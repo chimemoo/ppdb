a3fafbdd5b3649b246e25b17f7b9c3add89143fb
chimemoo <christ.16062@student.unsika.ac.id>
2018-04-18 20:25:21 +0700

    next

---
+++ vendor/ckeditor/config.js
@@ -0,0 +1,14 @@
+/**
+ * @license Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+CKEDITOR.editorConfig = function( config )
+{
+	config.filebrowserBrowseUrl = 'http://localhost/ppdb/vendor/kcfinder/browse.php?type=files';
+	config.filebrowserImageBrowseUrl = 'http://localhost/ppdb/vendor/kcfinder/browse.php?type=images';
+	config.filebrowserFlashBrowseUrl = 'http://localhost/ppdb/vendor/kcfinder/browse.php?type=flash';
+	config.filebrowserUploadUrl = 'http://localhost/ppdb/vendor/kcfinder/upload.php?type=files';
+	config.filebrowserImageUploadUrl = 'http://localhost/ppdb/vendor/kcfinder/upload.php?type=images';	
+	config.filebrowserFlashUploadUrl = 'http://localhost/ppdb/vendor/kcfinder/upload.php?type=flash';
+};
