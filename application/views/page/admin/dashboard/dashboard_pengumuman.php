0f5ec36d8cde6ac91f01aab7939dee1868bc8be7
chimemoo <christ.16062@student.unsika.ac.id>
2018-05-12 11:30:39 +0700

    11.30 12/05/2018

--- application/views/page/admin/dashboard/dashboard_pengumuman.php
+++ application/views/page/admin/dashboard/dashboard_pengumuman.php
@@ -139,7 +139,7 @@
               	</div>
               	<div class="modal-footer">
                 	<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                	<button type="submit" class="btn btn-primary">Save changes</button></form>
+                	<button type="submit" class="btn btn-primary">Simpan</button></form>
               	</div>
             </div>
         </div>
